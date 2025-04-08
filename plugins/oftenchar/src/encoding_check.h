979c6f9997be748ceb287117a40a576ac4abc4db
yr <yr186@qq.com>
2025-04-08 21:49:44 +0800

    release plugins/*

--- plugins/oftenchar/src/encoding_check.h
+++ plugins/oftenchar/src/encoding_check.h
@@ -4,7 +4,11 @@
 #include <string>
 
 bool is_valid_encoding(const std::string& text, const std::string& charset);
+
+#ifdef _WIN32
 bool is_valid_encoding_win(const std::string& text, const std::string& charset);
+#else
 bool is_valid_encoding_iconv(const std::string& text, const std::string& charset);
+#endif
 
 #endif
